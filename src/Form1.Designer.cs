b0e5cc5f3c6b7ad8cca85f361af8d710b9d9c2da
liubin <lb@qycloud.com>
2022-06-09 16:59:03 +0800

    init

---
+++ src/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace ProbabilityCalculation
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ThingName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Probability = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IsCheck = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ThingName,
+            this.Probability,
+            this.Value,
+            this.IsCheck});
+            this.dataGridView1.Location = new System.Drawing.Point(28, 26);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(444, 440);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // ThingName
+            // 
+            this.ThingName.HeaderText = "名称";
+            this.ThingName.Name = "ThingName";
+            // 
+            // Probability
+            // 
+            this.Probability.HeaderText = "比例(%)";
+            this.Probability.Name = "Probability";
+            // 
+            // Value
+            // 
+            this.Value.HeaderText = "价值";
+            this.Value.Name = "Value";
+            // 
+            // IsCheck
+            // 
+            this.IsCheck.HeaderText = "抽中是否暂停";
+            this.IsCheck.Name = "IsCheck";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(488, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "单次抽取价格";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(574, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(695, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(488, 104);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(306, 362);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(813, 104);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(299, 362);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(488, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "抽奖记录：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(813, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "中奖记录：";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(813, 44);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "清空记录";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1156, 485);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ThingName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Probability;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn IsCheck;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ src/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProbabilityCalculation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int count = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //生成抽奖列表
+            List<Thing> things = new List<Thing>();
+            //生成随机抽奖列表
+            List<Thing> randomThings = new List<Thing>();
+            //界面上的抽奖数据
+            List<Thing> list = new List<Thing>();
+            try
+            {
+                //抽奖列表数据
+                for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                {
+                    var thingName = dataGridView1.Rows[i].Cells[0].Value == null ? "" : dataGridView1.Rows[i].Cells[0].Value.ToString();
+                    decimal.TryParse(dataGridView1.Rows[i].Cells[1].Value?.ToString(), out decimal probability);
+                    decimal.TryParse(dataGridView1.Rows[i].Cells[2].Value?.ToString(), out decimal value);
+                    bool.TryParse(dataGridView1.Rows[i].Cells[3].Value?.ToString(), out bool isCheck);
+                    list.Add(new Thing
+                    {
+                        ThingName = thingName ?? "",
+                        Probability = probability,
+                        Valuse = value,
+                        IsCheck = isCheck,
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("请输入正确的抽奖数据");
+                return;
+            }
+            if (list.Any(x => x.Probability == 0 && x.IsCheck))
+            {
+                MessageBox.Show("暂停的抽奖概率不能为0");
+                return;
+            }
+            if (list.All(x => !x.IsCheck))
+            {
+                MessageBox.Show("请选择抽中暂停的奖品");
+                return;
+            }
+            var min = list.Where(x => x.Probability > 0).Select(x => x.Probability).Min();
+            var d = 0;
+            var b = 1;
+            //最小的概率是否是小数，小数的话按10的倍数转成整数
+            while (true && min.ToString().Contains("."))
+            {
+                b = b * 10;
+                if ((min * d).ToString().Contains(".00"))
+                {
+                    d = b;
+                    break;
+                }
+            }
+            //根据概率生成对应的奖品数量
+            foreach (var item in list)
+            {
+                for (int i = 0; i < item.Probability * d; i++)
+                {
+                    things.Add(item);
+                }
+            }
+            //随机打乱奖品顺序
+            while (things.Count > 0)
+            {
+                Random r = new Random();
+                var index = r.Next(0, things.Count - 1);
+                randomThings.Add(things[index]);
+                things.RemoveAt(index);
+            }
+            richTextBox1.Clear();
+            while (true)
+            {
+                count += 1;
+                Random random = new Random();
+                int i = random.Next(0, randomThings.Count - 1);
+                var model = randomThings[i];
+                richTextBox1.Text = richTextBox1.Text.Insert(0, $"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} {count}-{i}-{model.ThingName}\n");
+                if (model.IsCheck)//代表抽中自己想要的奖品
+                {
+                    if (!string.IsNullOrEmpty(textBox1.Text))
+                    {
+                        if (decimal.TryParse(textBox1.Text, out decimal price))
+                        {
+                            var cost = count * price;
+                            richTextBox2.Text = richTextBox2.Text.Insert(0, $"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} 总共抽取了{count}次抽中了{model.ThingName},花费{cost},{(model.Valuse - cost <= 0 ? "亏" : "赚")}了{Math.Abs(model.Valuse - cost)}。\n");
+                        }
+                        else
+                        {
+                            richTextBox2.Text = richTextBox2.Text.Insert(0, $"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} 总共抽取了{count}次抽中了{model.ThingName}。\n");
+                        }
+                    }
+                    else
+                    {
+                        richTextBox2.Text = richTextBox2.Text.Insert(0, $"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} 总共抽取了{count}次抽中了{model.ThingName}。\n");
+                    }
+                    count = 0;
+                    break;
+                } 
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            richTextBox2.Clear();
+        }
+    }
+
+    public class Thing
+    {
+        /// <summary>
+        /// 名字
+        /// </summary>
+        public string? ThingName { get; set; }
+        /// <summary>
+        /// 概率
+        /// </summary>
+        public decimal Probability { get; set; }
+
+        /// <summary>
+        /// 概率
+        /// </summary>
+        public int ProbabilityNumber { get; set; }
+        /// <summary>
+        /// 价值
+        /// </summary>
+        public decimal Valuse { get; set; }
+        /// <summary>
+        /// 抽中是否暂停
+        /// </summary>
+        public bool IsCheck { get; set; }
+    }
+}
